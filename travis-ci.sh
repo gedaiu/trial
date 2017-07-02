536a8c4caf031ecf895829f14abaa64a2b3f20e9
Szabo Bogdan <bogdan.szabo@altom.ro>
2017-07-02 20:20:35 +0300

    update travis

--- travis-ci.sh
+++ travis-ci.sh
@@ -3,12 +3,13 @@ set -e -x -o pipefail
 
 # test for successful 32-bit build
 if [ "$DC" == "dmd" ]; then
-	dub build --combined --arch=x86
+	dub build :runner --arch=x86
+	dub build :lifecycle --arch=x86
 	dub clean --all-packages
 fi
 
 # test for successful release build
-dub build --combined -b release --compiler=$DC
+dub build :runner -b release --compiler=$DC
 dub clean --all-packages
 
 # run unit tests
