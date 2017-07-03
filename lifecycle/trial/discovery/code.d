56484d562cc95c774a4757f1adbbd2af47ed5384
Bogdan Szabo <bogdan.szabo@altom.ro>
2017-07-03 16:34:11 +0300

    add missing file

---
+++ lifecycle/trial/discovery/code.d
@@ -0,0 +1,22 @@
+module trial.discovery.code;
+
+import std.algorithm;
+import std.string;
+import std.range;
+import std.file;
+import std.stdio;
+import std.conv;
+
+string getModuleName(string fileName) {
+	auto file = File(fileName);
+
+	auto moduleLine = file.byLine()
+		.map!(a => a.to!string)
+		.filter!(a => a.startsWith("module"));
+
+	if(moduleLine.empty) {
+		return "";
+	}
+
+	return moduleLine.front.split(' ')[1].split(";")[0];
+}
